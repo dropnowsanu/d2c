93c8a426fd45acb2ac35f252a7cc8d303b306da4
Sanu Islam <dropnow.sanu@gmail.com>
2023-06-05 05:35:22 -0400

    Meta Update

---
+++ app/Providers.js
@@ -0,0 +1,12 @@
+"use client";
+
+import { ThemeProvider } from "@material-tailwind/react";
+
+
+export default function Providers({ children }) {
+    return (
+        <ThemeProvider>
+            {children}
+        </ThemeProvider>
+    )
+}
