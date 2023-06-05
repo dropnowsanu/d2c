d26dac6852cca2a6c9bc5b5519987f0f6251a631
Sanu Islam <dropnow.sanu@gmail.com>
2023-06-05 11:05:11 -0400

    Navbar Add to component

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Providers from './Providers'
+import Headers from './components/Headers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +15,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <Providers>
+          <Headers />
           {children}
         </Providers>
       </body>
